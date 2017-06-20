f11d52d43ce4417210deae0065fd4028f93abf88
Gabriel Russell <gabriel.russell@mongodb.com>
2017-06-20 14:03:35 -0400

    bare sessions_collection_standalone

---
+++ src/mongo/db/sessions_collection_standalone.cpp
@@ -0,0 +1,67 @@
+/**
+ *    Copyright (C) 2017 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/client/dbclientinterface.h"
+#include "mongo/client/query.h"
+#include "mongo/db/dbdirectclient.h"
+#include "mongo/db/clientcursor.h"
+#include "mongo/db/operation_context.h"
+#include "mongo/db/sessions_collection_standalone.h"
+#include "mongo/platform/basic.h"
+#include "mongo/stdx/functional.h"
+
+namespace mongo {
+
+StatusWith<LogicalSessionRecord> StandaloneSessionsCollection::fetchRecord(LogicalSessionId lsid) {
+    OperationContext* opCtx = cc().getOperationContext();
+    DBDirectClient client(opCtx);
+    Query lsidQuery(BSON("Id" << lsid.toString()));
+    std::unique_ptr<DBClientCursor> cursor = client.query("admin.system.sessions", lsidQuery, 1);
+    if (!cursor->more()) {
+        return {ErrorCodes::NoSuchSession, "No matching record in the sessions collection"};
+    }
+    return LogicalSessionRecord::parse(cursor->next());
+}
+
+Status StandaloneSessionsCollection::insertRecord(LogicalSessionRecord record) {
+    OperationContext* opCtx = cc().getOperationContext();
+    DBDirectClient client(opCtx);
+    client.insert("admin.system.sessions", record.toBSON());
+    auto result = client.getLastError();
+    return Status::OK();
+}
+
+LogicalSessionIdSet StandaloneSessionsCollection::refreshSessions(LogicalSessionIdSet sessions) {
+    return sessions;
+}
+
+void StandaloneSessionsCollection::removeRecords(LogicalSessionIdSet sessions) {
+    return;
+}
+
+}  // namespace mongo
