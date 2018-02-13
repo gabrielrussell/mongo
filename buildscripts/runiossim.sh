4f1ab626e84a2b18546906d7d6784b0e848ca7a7
Gabriel Russell <gabriel.russell@mongodb.com>
2018-02-13 16:20:16 -0500

    refactoring runiossim.sh

--- buildscripts/runiossim.sh
+++ buildscripts/runiossim.sh
@@ -7,25 +7,27 @@ if [ "$#" != "3" ]; then
 fi
 
 set -o verbose
+set -o errexit
 
 DEVICE="$1"
 RUNTIME="$2"
 TEST="$3"
 
+cleanup() {
+    echo "Shutting down simulator"
+    xcrun simctl shutdown mongo-sim
+
+    echo "Erasing simulator"
+    xcrun simctl erase mongo-sim
+}
+
 echo "Creating simulator"
 xcrun simctl create mongo-sim "com.apple.CoreSimulator.SimDeviceType.$DEVICE" "com.apple.CoreSimulator.SimRuntime.$RUNTIME"
+
+trap cleanup EXIT
 
 echo "Booting simulator"
 xcrun simctl boot mongo-sim
 
 echo "Spawning test program in simulator"
 xcrun simctl spawn mongo-sim "$TEST" --tempPath /data
-RC=$?
-
-echo "Shutting down simulator"
-xcrun simctl shutdown mongo-sim
-
-echo "Erasing simulator"
-xcrun simctl erase mongo-sim
-
-exit $RC
