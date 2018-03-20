514d40c34e0544188aae6cb958418c70d1abeb1f
Gabriel Russell <gabriel.russell@mongodb.com>
2018-03-20 15:05:15 -0400

    SERVER-34005 XXX wip

--- buildscripts/setup-android-toolchain.sh
+++ buildscripts/setup-android-toolchain.sh
@@ -7,6 +7,22 @@ if [ -z "$PYTHON" ] ; then
     PYTHON=`which python`
 fi
 
+ARCH=$1; shift
+
+case $ARCH in
+    arm64)
+        SYSTEM_IMAGE="system-images;android-24;google_apis;arm64-v8a"
+        ;;
+    x86_64)
+        SYSTEM_IMAGE="system-images;android-24;google_apis;x86_64"
+        ;;
+    *)
+        echo "usage: $0 <arch>" >&2
+        exit 1
+        ;;
+esac
+
+
 SDK_ROOT=$PWD/android_sdk
 mkdir $SDK_ROOT
 
@@ -20,10 +36,10 @@ API_VERSION=24
     SDK_PACKAGE=sdk-tools-linux-3859397.zip
     curl -O https://dl.google.com/android/repository/$SDK_PACKAGE
     unzip $SDK_PACKAGE
-    echo y | ./tools/bin/sdkmanager "platforms;android-24" "emulator" "ndk-bundle" "platform-tools" "build-tools;23.0.3" "system-images;android-24;google_apis;arm64-v8a"
+    echo y | ./tools/bin/sdkmanager "platforms;android-24" "emulator" "ndk-bundle" "platform-tools" "build-tools;23.0.3" $SYSTEM_IMAGE
 )
 
-$PYTHON $SDK_ROOT/ndk-bundle/build/tools/make_standalone_toolchain.py --arch arm64 --api $API_VERSION  --stl=libc++ --force  --install-dir $TOOLCHAIN
+$PYTHON $SDK_ROOT/ndk-bundle/build/tools/make_standalone_toolchain.py --arch $ARCH --api $API_VERSION  --stl=libc++ --force  --install-dir $TOOLCHAIN
 
 echo SDK_ROOT=${SDK_ROOT}
 echo TOOLCHAIN=${TOOLCHAIN}
